04149767bac8e959a5bbf9acf439566f68d87d03
GraeWolf <kmccuddy@protonmail.com>
2024-02-24 10:58:39 -0500

    added always show hidden files

--- .config/nvim/lua/plugins/neo-tree.lua
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -6,4 +6,27 @@ return {
        "nvim-tree/nvim-web-devicons",
        "MunifTanjim/nui.nvim",
     },
+    config = function()
+        require("neo-tree").setup({
+            widnow = {
+                position = "left",
+                width = 5,
+            },
+            filesystem = {
+                filtered_items = {
+                    visible = true,
+                    show_hidden_count = true,
+                    hide_dotfiles = false,
+                    hide_gitignored = true,
+                    hide_by_name = {
+                        -- '.git',
+                        -- '.DS_Store',
+                        -- 'thumbs.db'
+                    },
+                    never_show = {},
+                },
+            },
+
+        })
+    end
 }
