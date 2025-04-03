9b1ce03c0791236299d11b5364e1251e1d4d3af8
GraeWolf <kmccuddy@protonmail.com>
2025-04-03 17:56:38 -0400

    new setup

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,8 +1,53 @@
--- leader
+-- Leader
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
--- Tab / Indendentation
+-- Line Numbers
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+-- Enable mouse mode
+vim.opt.mouse = 'a'
+
+-- Don't show the mode, since it's already in the status line
+vim.opt.showmode = false
+
+-- Sync clipboard between OS and Neovim.
+vim.schedule(function()
+  vim.opt.clipboard = 'unnamedplus'
+end)
+
+-- Enable break indent
+vim.opt.breakindent = true
+
+-- Save undo history
+vim.opt.undofile = true
+
+-- Case-insensitive searching
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+-- Decrease update time
+vim.opt.updatetime = 250
+
+-- Decrease mapped sequence wait time
+vim.opt.timeoutlen = 300
+
+-- Configure how new splits should be opened
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+
+-- Sets how neovim will display certain whitespace characters in editor.
+vim.opt.list = true
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+
+-- Preview substitutions live, as you type!
+vim.opt.inccommand = 'split'
+
+-- Show which line your cursor is on
+vim.opt.cursorline = true
+
+-- Tab / Indentation
 vim.opt.expandtab = true
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
@@ -10,26 +55,20 @@ vim.opt.softtabstop = 4
 vim.opt.smartindent = true
 vim.opt.wrap = false
 
--- Search
---vim.opt.hlseach = false
-vim.opt.ignorecase = true
---vim.opt.insearch = true
-
 -- Appearance
 vim.opt.termguicolors = true
 vim.opt.colorcolumn = '80'
-vim.opt.signcolumn = "yes"
+vim.opt.signcolumn = 'yes'
 vim.opt.scrolloff = 10
 
 -- Behavior
-vim.opt.number = true
-vim.opt.relativenumber = true
-vim.opt.splitbelow = true
-vim.opt.splitright = true
-vim.opt.clipboard = "unnamedplus"
-vim.opt.virtualedit = "block"
+vim.opt.virtualedit = 'block'
 
 -- which-key options
-vim.o.timeout = true
-vim.o.timeoutlen = 300
-vim.o.mouse = ''
+vim.opt.timeout = true
+vim.opt.timeoutlen = 300
+vim.opt.mouse = ''
+
+-- Sets how neovim will display certain whitespace characters in the editor
+-- vim.opt.list = true
+-- vim.opt.listchars = { tab = '>> ', trail = '.', nbsp = '␣' }
