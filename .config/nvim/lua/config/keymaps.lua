59e3b1dcf6d28518a3f366deff40d1a9771e4055
GraeWolf <kmccuddy@protonmail.com>
2025-04-12 15:47:04 -0400

    added vim key to switch open buffer winodws

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -6,3 +6,14 @@ end, { desc = "Open Diagnostic in Float" })
 vim.keymap.set("n", "<leader>cf", function()
 	require("conform").format({ lsp_format = "fallback" })
 end, { desc = "Format current file" })
+
+-- Panel Navigation
+vim.keymap.set("n", "<C-h>", "<C-w>h", opts) -- Navigate Left
+vim.keymap.set("n", "<C-j>", "<C-w>j", opts) -- Navigate Down
+vim.keymap.set("n", "<C-k>", "<C-w>k", opts) -- Navigate Up
+vim.keymap.set("n", "<C-l>", "<C-w>l", opts) -- Navigate Right
+
+-- Window management
+vim.keymap.set("n", "<leader>sv", ":vsplit<CR>", opts) -- Split Vertically
+vim.keymap.set("n", "<leader>sh", ":split<CR>", opts) -- Split Horizontally
+vim.keymap.set("n", "<leader>sm", ":MaximizerToggle<CR>", opts) --Toggle Minimize
