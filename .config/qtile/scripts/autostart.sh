19b02227b6e610ace98f4642749f9f77138a481c
GraeWolf <kmccuddy@protonmail.com>
2025-06-28 10:56:21 -0400

    initial commit

---
+++ .config/qtile/scripts/autostart.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+#~/.config/qtile/scripts/locker.sh &
+picom --config ~/.config/picom/picom.conf &
+#~/bin/wallpaper &
+nm-applet &
+volumeicon &
+#~/.screenlayout/qtile_layout.sh &
+#megasync &
+#dropbox start &
+pipewire &
